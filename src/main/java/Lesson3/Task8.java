44268984155d0cdb8de73b9092c066ef5525c04e
Asad <asad.gadzhiev07@gmail.com>
2021-12-08 15:00:42 +0300

    Home work lesson 3 task8

--- src/main/java/Lesson3/Task8.java
+++ src/main/java/Lesson3/Task8.java
@@ -1,26 +1,48 @@
 package Lesson3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task8 {
     public static void main(String[] args) {
         int[] arr1 = {10,20,30,40,50};
 
-        turnLeft(arr1, 2);
+        turnArr(arr1, 2);
 
     }
 
-    private static void turnLeft(int[] arr, int n) {
-        int c = arr[0];
-        int b = 0;
-        for (int i = 0; i < arr.length-1; i++) {
-            if (i+n <= arr.length)
-            {b = arr[i+n];
-            arr[i+n]= c;
-            c = b;}
-                //System.out.print(arr[i+n]);}
-            else{arr[i] = arr[arr.length - 1];
-            }
+    private static void turnArr(int[] arr, int n) {
+
+        if (n % arr.length > 0 ){
+            shiftRight(arr, n);
+        } else {shiftLeft(arr, n);}
+        System.out.println(Arrays.toString(arr));
 
         }
 
+
+    private static void shiftRight(int[] arr, int n) {
+
+
+        for (int i = 0; i < n; i++) {
+            int temp = arr[arr.length - 1];
+            for (int j = arr.length-1; j >0 ; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = temp;
+        }
     }
+    private static void shiftLeft(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            int temp = arr[0];
+            for (int j = 0; j < arr.length-1 ; j++) {
+                arr[j] = arr[j+1];
+            }
+            arr[arr.length-1] = temp;
+        }
+
+    }
+
+
+
 }
